8a59ce3babcbe9ef7aa415b55905834a760980c4
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-02-08 01:06:40 -0600

    add async to routes using express-async-errors for asynchronous requests

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
@@ -17,11 +18,14 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all('*', () => {
+// app.all('*', async (req, res, next) => {
+app.all('*', async (req, res) => {
   // express will capture error and send it off to the middleware,
   // which will take the status code and call the serialize function,
   // then generate a response and send it back to client.
-  throw new NotFoundError();
+  // throw new NotFoundError(); // only for synchronous (without express-async-errors package)
+  // next(new NotFoundError()); // for asynchronous (without express-async-errors package)
+  throw new NotFoundError(); // express-async-errors allows upgrades it to asynchronous
 });
 
 // middleware
