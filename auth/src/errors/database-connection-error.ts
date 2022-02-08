0837e114a76a33d6b07c75ecd647c7bb9bbbf635
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-02-08 00:39:48 -0600

    implement a custom error class and have specific errors extend it

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,11 +1,11 @@
-import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
 
-export class DatabaseConnectionError extends Error {
+export class DatabaseConnectionError extends CustomError {
   statusCode = 500;
   reason = 'Error connecting to database';
 
   constructor() {
-    super();
+    super('Error connecting to database');
 
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
   }
