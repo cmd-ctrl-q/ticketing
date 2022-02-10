1e8e1f7c084508a1fd1198b38145229828669697
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-02-09 23:12:11 -0600

    create static method to compare stored and supplied passwords

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -1,8 +1,22 @@
 import { scrypt, randomBytes } from 'crypto';
 import { promisify } from 'util';
+import { BadRequestError } from '../errors/bad-request-error';
+
+const scryptAsync = promisify(scrypt);
 
 export class Password {
-  static toHash(password: string) {}
+  static async toHash(password: string) {
+    const salt = randomBytes(8).toString('hex');
+    const buf = (await scryptAsync(password, salt, 64)) as Buffer;
 
-  static compare(storedPassword: string, suppliedPassword: string) {}
+    return `${buf.toString('hex')}.${salt}`;
+  }
+
+  static async compare(storedPassword: string, suppliedPassword: string) {
+    // split the hash and the salt
+    const [hashedPassword, salt] = storedPassword.split('.');
+    const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+    return buf.toString('hex') === hashedPassword;
+  }
 }
