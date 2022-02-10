4968c56254b9ca4a0ae126eff1e81981d89aa99a
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-02-09 23:04:58 -0600

    create password class using scrypt for hashing and salting passwords

---
+++ auth/src/services/password.ts
@@ -0,0 +1,8 @@
+import { scrypt, randomBytes } from 'crypto';
+import { promisify } from 'util';
+
+export class Password {
+  static toHash(password: string) {}
+
+  static compare(storedPassword: string, suppliedPassword: string) {}
+}
