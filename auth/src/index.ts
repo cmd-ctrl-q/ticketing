cfca779981dc3e9eef557ec8d0ffcb8769aee6d7
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-02-10 22:07:24 -0600

    install cookie-session package and add session support

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -9,9 +10,17 @@ import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
+import { isTypeQueryNode } from 'typescript';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // dont encrypt
+    secure: true, // http connection
+  })
+);
 
 // routes
 app.use(currentUserRouter);
