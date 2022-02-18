a8b22169f758058923d52f03d1be488c13342f7f
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-02-18 02:03:37 -0600

    add validation to signin route and check for errors in body

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,31 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('hi there from signin');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password'),
+  ],
+  (req: Request, res: Response) => {
+    // check errors from express-validator
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+
+    // does user with email exist?
+
+    // do the passwords match?
+
+    // user is now logged in; send user JWT inside a cookie
+  }
+);
 
 export { router as signinRouter };
