f2d9220c1a198e3e21244747ceb2a850dd1caf92
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-02-19 10:51:53 -0600

    replace inline error handling validation request with middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
@@ -13,17 +13,10 @@ router.post(
       .notEmpty()
       .withMessage('You must supply a password'),
   ],
+  validateRequest,
   (req: Request, res: Response) => {
-    // check errors from express-validator
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     // does user with email exist?
-
     // do the passwords match?
-
     // user is now logged in; send user JWT inside a cookie
   }
 );
