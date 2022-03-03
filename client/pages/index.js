a0d1416e1f197c7b18f8b1691e93a7464348920a
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-03-03 12:54:20 -0600

    use getInitialProps for server or browser request

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,37 @@
-const LandingPage = ({ color }) => {
-  console.log('I am in the component,', color);
-  return <div>landing page...</div>;
+import axios from 'axios';
+
+// Client Browser
+const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser').catch((err) => {
+  //   console.log(err.message);
+  // });
+
+  return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = () => {
-  console.log('I am on the server!');
+// Browser or SSR
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    console.log(req.headers);
+    // on server, request base domain must be specified
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        withCredentials: true,
+        headers: req.headers,
+      }
+    );
 
-  // shows up on LandingPage as the initial props.
-  return { color: 'red' };
+    return data;
+  } else {
+    // on browser, request base domain doesn't have to be specified as it auto appends the headers
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
+  }
+
+  return {};
 };
 
 export default LandingPage;
