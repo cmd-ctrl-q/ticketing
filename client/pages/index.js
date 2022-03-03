033d338fcf23aadcfb65d8608227b9b5259f81b0
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-03-02 18:38:34 -0600

    fetch initial props from server via getInitialProps Next.js method

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,13 @@
-const Landing = () => {
+const LandingPage = ({ color }) => {
+  console.log('I am in the component,', color);
   return <div>landing page...</div>;
 };
 
-export default Landing;
+LandingPage.getInitialProps = () => {
+  console.log('I am on the server!');
+
+  // shows up on LandingPage as the initial props.
+  return { color: 'red' };
+};
+
+export default LandingPage;
