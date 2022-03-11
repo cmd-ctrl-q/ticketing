db026f7a45db4fd0ea7065c76378cd5f9e8ce300
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-03-11 14:24:53 -0600

    await event publication

--- nats-test/src/events/base-publisher.ts
+++ nats-test/src/events/base-publisher.ts
@@ -1,4 +1,5 @@
 import { Stan } from 'node-nats-streaming';
+import { kill } from 'process';
 import { Subjects } from './subjects';
 
 interface Event {
@@ -14,9 +15,16 @@ export abstract class Publisher<T extends Event> {
     this.client = client;
   }
 
-  publish(data: T['data']) {
-    this.client.publish(this.subject, JSON.stringify(data), () => {
-      console.log('Event published.');
+  publish(data: T['data']): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.client.publish(this.subject, JSON.stringify(data), (err) => {
+        if (err) {
+          return reject(err);
+        }
+
+        console.log('Event published to subject:', this.subject);
+        resolve();
+      });
     });
   }
 }
