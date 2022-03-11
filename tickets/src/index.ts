af2e8b1cae9f4feb4c5a57b4b35bbc70a92db7ea
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-03-11 16:02:33 -0600

    implement NATS graceful shutdown

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,15 +1,22 @@
 import mongoose from 'mongoose';
+import nats from 'node-nats-streaming';
+import { natsWrapper } from './nats-wrapper';
 
 import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
-
-  if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be defined.');
-  }
+  if (!process.env.MONGO_URI) throw new Error('MONGO_URI must be defined.');
 
   try {
+    await natsWrapper.connect('ticketing', 'asdfasdf', 'http://nats-srv:4222');
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close()); // interrupt signal
+    process.on('SIGTERM', () => natsWrapper.client.close()); // terminate signal
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (err) {
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@teds-tickets/common';
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -26,6 +28,14 @@ router.post(
       userId: req.currentUser!.id,
     });
     await ticket.save();
+
+    // publish the newly created ticket to the nat streaming service
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: req.currentUser!.id,
+    });
 
     res.status(201).send(ticket);
   }
