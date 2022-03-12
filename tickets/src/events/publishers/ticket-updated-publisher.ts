6c312e77b62eefd756bcaa6f275fa3fd727e224e
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-03-12 15:30:48 -0600

    Create NATS subject for updating a ticket; emit a ticket update to event bus

---
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketUpdatedEvent } from '@teds-tickets/common';
+
+export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+}
