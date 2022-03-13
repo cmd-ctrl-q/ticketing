b31fb4d3ada96ebb12f735081102bda6cd25cd3c
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-03-13 17:23:36 -0500

    define the orders model and a ticket model for the orders microservice

---
+++ orders/src/models/order.ts
@@ -0,0 +1,66 @@
+import mongoose from 'mongoose';
+import { OrderStatus } from '@teds-tickets/common';
+import { TicketDoc } from './ticket';
+
+// An interface that describes the properties that are required to create an Order
+interface OrderAttrs {
+  userId: string; // typescript types
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+// An instance of an order.
+// An interface that describes the properties that (a saved) Order Document has.
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+// An interface that describes the properties that an Order Model has.
+// Purpose is for type checking the arguments when creating a new document.
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      // Set to a value in the enum
+      enum: Object.values(OrderStatus),
+      // Not necessary
+      default: OrderStatus.Created,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ticket',
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+
+export { Order };
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,25 @@
+import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@teds-tickets/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({ msg: 'new orders route' });
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      // check that id is a valid mongo id; (assumes always using mongo db)
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('ticketId must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({ msg: 'new orders route' });
+  }
+);
 
 export { router as createOrderRouter };
