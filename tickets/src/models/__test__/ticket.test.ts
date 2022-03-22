38ccb81590a301c351e0e610c6e1b9d3b2cdee5d
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-03-22 12:24:36 -0500

    test Ticket versioning

---
+++ tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,33 @@
+import { Ticket } from '../ticket';
+
+it('implements optimistic concurrency control', async () => {
+  // Create instance of ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 100,
+    userId: '123',
+  });
+
+  // Save ticket to db
+  await ticket.save();
+
+  // Fetch ticket twice
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  // Make two separate changes of the fetched tickets
+  firstInstance!.set({ price: 101 });
+  secondInstance!.set({ price: 102 });
+
+  // Save the first fetched ticket (version: 1)
+  await firstInstance!.save();
+
+  // Save the second fetched ticket (outdated version ) and expect error
+  try {
+    await secondInstance!.save();
+  } catch (err) {
+    return;
+  }
+
+  throw new Error('Should not reach this point.');
+});
